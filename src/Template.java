b449131705086d5029038c31d8e4e7cc1cb29340
dhanan27599 <dhananjayan.dayalan@gmail.com>
2021-04-04 12:12:22 +0530

    Template

--- src/Template.java
+++ src/Template.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import java.io.*;
 public class Template {
+    private static final FastScanner fs = new FastScanner();
+
     public static void main(String[] args) throws IOException {
 
     }
