9107d61e36b68e6b1b2b13746af442ff9b3a3729
dhanan27599 <dhananjayan.dayalan@gmail.com>
2021-04-04 12:09:00 +0530

    Template

--- src/Template.java
+++ src/Template.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 public class Template {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
     }
 
