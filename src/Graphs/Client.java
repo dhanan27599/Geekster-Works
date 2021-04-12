b7689f17c45458ff2099547322150a145bc1abd0
dhanan27599 <dhananjayan.dayalan@gmail.com>
2021-04-12 21:30:59 +0530

    Disjoint Set and Kruskals

--- src/Graphs/Client.java
+++ src/Graphs/Client.java
@@ -16,25 +16,25 @@ public class Client {
 //        g.addVertex("G");
 //
 //        g.addEdge("A", "B", 28);
-////        g.addEdge("B", "A", 28);
+//        g.addEdge("B", "A", 28);
 //        g.addEdge("B", "C", 16);
-////        g.addEdge("C", "B", 16);
+//        g.addEdge("C", "B", 16);
 //        g.addEdge("C", "D", 12);
-////        g.addEdge("D", "C", 12);
+//        g.addEdge("D", "C", 12);
 //        g.addEdge("D", "E", 22);
-////        g.addEdge("E", "D", 22);
+//        g.addEdge("E", "D", 22);
 //        g.addEdge("E", "F", 25);
-////        g.addEdge("F", "E", 25);
+//        g.addEdge("F", "E", 25);
 //        g.addEdge("F", "A", 10);
-////        g.addEdge("A", "F", 10);
+//        g.addEdge("A", "F", 10);
 //        g.addEdge("B", "G", 14);
-////        g.addEdge("G", "B", 14);
+//        g.addEdge("G", "B", 14);
 //        g.addEdge("D", "G", 18);
-////        g.addEdge("G", "D", 18);
+//        g.addEdge("G", "D", 18);
 //        g.addEdge("G", "E", 24);
-////        g.addEdge("E", "G", 24);
+//        g.addEdge("E", "G", 24);
 //
-////        System.out.println(g);
+//        System.out.println(g);
 //
 //        Graph graph = GraphUtil.prismMst(g, "A");
 //        System.out.println(graph.calculateCost("A", new HashSet<>(), 0));
